caf9a73b9ee56a65e7c5f2b292009fecaec9de3f
Hafizh Arrizqa <49109989+adha152@users.noreply.github.com>
2020-12-06 22:14:27 +0700

    Update compilekernel.sh
    Change user host

--- termux/compilekernel.sh
+++ termux/compilekernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export KBUILD_BUILD_USER=xyzuan
-export KBUILD_BUILD_HOST=xyzscape
+export KBUILD_BUILD_USER=fakeriz
+export KBUILD_BUILD_HOST=fakergang
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig
+    make -j$(nproc) O=out ARCH=arm64 surya-perf_defconfig
     make -j$(nproc) ARCH=arm64 O=out \
                                CROSS_COMPILE=aarch64-linux-gnu- \
                                CROSS_COMPILE_ARM32=arm-linux-gnueabi-
